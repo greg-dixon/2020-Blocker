4081e92d4f102b1be6a1c786d259a13de6469ca2
Stella Liao <stellaliao@Stellas-MacBook-Pro.local>
2020-10-08 17:16:27 -0400

    fjdsi

--- main.js
+++ main.js
@@ -12,40 +12,74 @@ const obj = {
     court: "Nachos",
     ballots: "ballots DO YOUR CIVIC DUTY AND VOTE",
     election: "Hunger Games",
-    // policy: "puppies",
-    // policies: "puppies",
     "Joe Biden": "Joe Shmoe Biden",
     Stock: "fish",
     stocks: "fish",
     census: "tacos",
+    officer: "avatar"
 }
+const hide = (remove, replace) => {
+    //massive function: iterate through object /replace all the words with our values
+    obj[remove] = replace;
 
-//massive function: iterate through object /replace all the words with our values
-for (let x in obj) {
-    if (document.body.innerHTML.includes(x)) {
-        alert('WARNING: The content on this page would have horrified and disturbed you and destroyed your faith in humanity. That is why we\'ve replaced those trigger words with something much more pleasant :)')
-        break
+    for (let x in obj) {
+        if (document.body.innerHTML.includes(x)) {
+            alert('WARNING: The content on this page would have horrified and disturbed you and destroyed your faith in humanity. That is why we\'ve replaced those trigger words with something much more pleasant :)')
+            break
+        }
+    }
+    for (let x in obj) {
+        let toBeRemoved = new RegExp(x, "gi");
+        document.body.innerHTML = document.body.innerHTML.replace(toBeRemoved,
+            obj[x]);
     }
 }
-for (let x in obj) {
-    let toBeRemoved = new RegExp(x, "gi")
-    document.body.innerHTML = document.body.innerHTML.replace(toBeRemoved,
-        obj[x])
-}
-
-function save_options() {
-    let removed = document.getElementById('remove').value;
-    let replaceWith = document.getElementById('replace').value;
-    obj[removed] = replaceWith;
-}
-document.getElementById('Submit').addEventListener('click',
-    save_options);
+hide();
+// document.addEventListener('DOMContentLoaded', function() {
+let button = document.querySelector('button')
+let newRemoved
+let newReplaced
 
 
-//have an alert that says the page is about 2020
+button.addEventListener('click', function() {
+    // let removed = document.querySelector('#remove').value;
+    // let replaceWith = document.querySelector('#replace').value;
+    newRemoved = $('#remove').val();
+    newReplaced = $('#replace').val();
+    obj[newRemoved] = newReplaced
+    hide(newRemoved, newReplaced)
+        // location.reload()
+
+    // console.log(removed, replaceWith)
+    // obj[newRemoved] = newReplaced;
+    // alert('button clicked')
+    // obj[removed] = replaceWith;
+    console.log(obj)
+
+})
+
+obj[newRemoved] = newReplaced
+
+
+// function update() {
+//     let removed = document.querySelector('#remove').value;
+//     let replaceWith = document.querySelector('#replace').value;
+//     console.log(removed, replaceWith)
+//         // alert('button clicked')
+//     obj[removed] = replaceWith;
+//     console.log(obj)
+// }
+
+// $('#SubmitButton').on("click", e => {
+//     e.preventDefault();
+//     update()
+
+// });
+// button.addEventListener('click', save_options);
+// })
+
+//have an alert that says the page is about 2020 --- DONE
 //replace certain words with pictures
 //button to toggle on/off
 
-// add icon
-
-// user options
+// add icon ---- DONE
--- options.js
+++ options.js
@@ -1,33 +1,43 @@
-// Saves options to chrome.storage
+document.addEventListener('DOMContentLoaded', function() {
+        let button = document.getElementById('SubmitButton')
+        console.log(button)
+        button.addEventListener('click', function() {
+                // let removed = document.querySelector('#remove').value;
+                // let replaceWith = document.querySelector('#replace').value;
+                // console.log(removed, replaceWith)
+                alert('button clicked')
+                    // return obj[removed] = replaceWith;
+            })
+            // button.addEventListener('click', save_options);
+    })
+    // function save_options() {
+    //     let removed = document.getElementById('remove').value;
+    //     let replaceWith = document.getElementById('replace').value;
+    //     // chrome.storage.sync.set({
+    //     obj[removed] = replaceWith;
+    //     // }, function() {
+    //     //     // Update status to let user know options were saved.
+    //     //     let status = document.getElementById('status');
+    //     //     status.textContent = 'Options saved!';
+    //     //     setTimeout(function() {
+    //     //         status.textContent = '';
+    //     //     }, 750);
+    //     // })
 
-function save_options() {
-    let removed = document.getElementById('remove').value;
-    let replaceWith = document.getElementById('replace').value;
-    // chrome.storage.sync.set({
-    obj[removed] = replaceWith;
-    // }, function() {
-    //     // Update status to let user know options were saved.
-    //     let status = document.getElementById('status');
-    //     status.textContent = 'Options saved!';
-    //     setTimeout(function() {
-    //         status.textContent = '';
-    //     }, 750);
-    // })
 
+// }
+// document.getElementById('Submit').addEventListener('click',
+//     save_options);
 
-}
-document.getElementById('Submit').addEventListener('click',
-    save_options);
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-// function restore_options() {
-//     // Use default value color = 'red' and likesColor = true.
-//     chrome.storage.sync.get({
-//         favoriteColor: 'red',
-//         likesColor: true
-//     }, function(items) {
-//         document.getElementById('color').value = items.favoriteColor;
+// // Restores select box and checkbox state using the preferences
+// // stored in chrome.storage.
+// // function restore_options() {
+// //     // Use default value color = 'red' and likesColor = true.
+// //     chrome.storage.sync.get({
+// //         favoriteColor: 'red',
+// //         likesColor: true
+// //     }, function(items) {
+// //         document.getElementById('color').value = items.favoriteColor;
 //         document.getElementById('like').checked = items.likesColor;
 //     });
 // }
